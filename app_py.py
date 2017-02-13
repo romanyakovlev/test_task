937beea874137d53a4087bf6d6f5f7b9d9fc76ac
romanyakovlev <yakovlevroman8@gmail.com>
2017-02-13 10:33:13 +0700

    indent fix

--- app_py.py
+++ app_py.py
@@ -89,5 +89,5 @@ if __name__ == "__main__":
     admin.add_view(UserModelView(User, db.session, url='/admin/users/', name="Пользователи"))
     admin.add_view(OrderModelView(Order, db.session, url='/admin/orders/', name="Заказы"))
     admin.add_view(StatisticView(url='/admin/statistics/', name="Отчёт"))
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
