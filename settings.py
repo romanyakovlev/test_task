05da7b2462221996eb92d24040b5efdc35bfda4d
romanyakovlev <yakovlevroman8@gmail.com>
2017-02-13 10:37:24 +0700

    add os

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from util import ListConverter
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
